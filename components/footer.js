f22d7f98efcd2cb9e6e12abe370716665a5fb5e1
yeobi01 <yeobi_01@naver.com>
2023-04-25 02:43:37 +0900

    [Next.js] footer 수정

--- components/footer.js
+++ components/footer.js
@@ -6,17 +6,25 @@ export default function Header(){
         <footer className="text-gray-600 body-font">
             <div className="bg-gray-100">
                 <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-                <Link href="/" legacyBehavior>
-                    <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg>
-                        <span className="ml-3 text-xl">김박사넷</span>
+                <Link href='/' legacyBehavior>
+                    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                    <svg viewBox="-50 -50 250 250" class="icon">
+                        <g>
+                            <path d="M74.2995 0.779663L0.581299 54.339L28.7391 141H119.86L146.259 59.7522C147.308 56.5244 146.159 52.9884 143.413 50.9934L74.2995 0.779663Z" fill="#0EA6A6"></path>
+                            <path d="M0.581543 54.3391L28.7394 141H84.5603L106.496 73.4897C107.545 70.2619 106.396 66.7259 103.65 64.7309L44.9641 22.0933L0.581543 54.3391Z" fill="#2BB1B1"></path>
+                            <path
+                                d="M69.6951 75.3093L20.7067 39.7173L0.581543 54.339L28.7394 141H54.0425L72.5408 84.0679C73.5897 80.8401 72.4407 77.3042 69.6949 75.3092L69.6951 75.3093Z"
+                                fill="#65C6C6"
+                            ></path>
+                            <path d="M1.2993 53.8176L0.581543 54.3391L28.7394 141H30.213L45.8494 92.876C46.8982 89.6483 45.7493 86.1122 43.0034 84.1173L1.2993 53.8176Z" fill="#7ACECE"></path>
+                        </g>
+                    </svg>
+                    <span className="text-3xl font-extrabold w-64">김멘토넷</span>
                     </a>
                 </Link>
 
-                <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 Tailblocks —
-                    <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+                <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 미니프로젝트 33팀 -
+                    <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">권기오 권민석 김민서 김상엽 최현식</a>
                 </p>
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                     <a className="text-gray-500">
