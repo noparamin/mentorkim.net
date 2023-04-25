2ad55dd9610f35de81a9e8f8a1f547effed789c0
noparamin <noparamin@naver.com>
2023-04-25 20:20:41 +0900

    style/멘토 분야 태그 높이 축소

--- components/information/tag.js
+++ components/information/tag.js
@@ -2,19 +2,19 @@ export default function Tag() {
     return (
         <>
             <div class="flex md:ml-auto md:mr-0 mx-auto items-center flex-shrink-0 space-x-4">
-                <button class="bg-gray-100 inline-flex py-2 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
                     <span class="title-font font-medium">국내 멘토</span>
                 </button>
-                <button class="bg-gray-100 inline-flex py-2 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
                     <span class="title-font font-medium">기술 멘토</span>
                 </button>
-                <button class="bg-gray-100 inline-flex py-2 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
                     <span class="title-font font-medium">백엔드</span>
                 </button>
-                <button class="bg-gray-100 inline-flex py-2 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
                     <span class="title-font font-medium">빅데이터</span>
                 </button>
-                <button class="bg-gray-100 inline-flex py-2 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
                     <span class="title-font font-medium">클라우드</span>
                 </button>
             </div>
--- components/information/tag2.js
+++ components/information/tag2.js
@@ -2,19 +2,19 @@ export default function Tag2() {
     return (
         <>
             <div class="flex md:ml-auto md:mr-0 mx-auto items-center flex-shrink-0 space-x-4">
-                <button class="bg-gray-100 inline-flex py-2 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
                     <span class="title-font font-medium">기술 멘토</span>
                 </button>
-                <button class="bg-gray-100 inline-flex py-2 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
                     <span class="title-font font-medium">국내 멘토</span>
                 </button>
-                <button class="bg-gray-100 inline-flex py-2 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
                     <span class="title-font font-medium">모바일 APP</span>
                 </button>
-                <button class="bg-gray-100 inline-flex py-2 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
                     <span class="title-font font-medium">AWS</span>
                 </button>
-                <button class="bg-gray-100 inline-flex py-2 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
                     <span class="title-font font-medium">리액트 네이티브</span>
                 </button>
             </div>
