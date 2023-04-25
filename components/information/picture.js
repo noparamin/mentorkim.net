88062a06c9d09cf50abc23f7e6a194c258a742c5
yeobi01 <yeobi_01@naver.com>
2023-04-25 16:25:08 +0900

    [Next.js] 김멘토넷 멘토 리스트 수정

---
+++ components/information/picture.js
@@ -0,0 +1,7 @@
+export default function Picture(){
+    return (
+        <div className="flex w-full h-72 bg-teal-400 font-sans font-extrabold text-5xl text-white justify-center items-center">
+            SW Maestro의 멘토들 정보!!
+        </div>
+    )
+}
--- pages/mentorlist.js
+++ pages/mentorlist.js
@@ -1,6 +1,7 @@
 import MentorListInformation from "@/components/information/mentor-list";
 import Head from 'next/head'
 import Layout from '../components/layout'
+import Picutre from '../components/information/picture'
 
 export default function MentorList() {
     return (
@@ -10,6 +11,7 @@ export default function MentorList() {
           <meta name="description" content="김멘토넷" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
+        <Picutre />
         <MentorListInformation/>
       </Layout>
     )
