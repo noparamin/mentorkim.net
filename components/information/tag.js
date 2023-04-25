9832eb2142e2828fe8d872a42fa6df10d995e0a0
yeobi01 <yeobi_01@naver.com>
2023-04-25 23:34:09 +0900

    멘토리스트 페이지 컴포넌트화

--- components/information/tag.js
+++ components/information/tag.js
@@ -1,27 +1,24 @@
-export default function Tag() {
+const mentorInfo = require('../mentorInfo');
+
+export default function Tag(props) {
+    let mentor = {};
+    let skill = [];
+    if(props.id){
+        mentor = mentorInfo[props.id];
+        skill = mentor.skill;
+    }
+
     return (
         <>
             <div class="flex md:ml-auto md:mr-0 mx-auto items-center flex-shrink-0 space-x-4">
-                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
-                    <span class="title-font font-medium">국내 멘토</span>
-                </button>
-                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
-                    <span class="title-font font-medium">기술 멘토</span>
-                </button>
-                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
-                    <span class="title-font font-medium">백엔드</span>
-                </button>
-                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
-                    <span class="title-font font-medium">빅데이터</span>
-                </button>
-                <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
-                    <span class="title-font font-medium">클라우드</span>
-                </button>
+                {
+                    skill.map((tag, index) => (
+                        <button class="bg-gray-100 inline-flex py-1 px-3 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                            <span class="title-font font-medium">{tag}</span>
+                        </button>
+                    ))
+                }
             </div>
         </>
     )
 }
-
-
-
-
