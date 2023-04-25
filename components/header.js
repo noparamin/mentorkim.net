4f24b5b2c92fc7caa2855e0ad840ed0c80637ea1
yeobi01 <yeobi_01@naver.com>
2023-04-25 23:34:58 +0900

    헤더 수정

--- components/header.js
+++ components/header.js
@@ -32,7 +32,7 @@ export default function Header(){
                             <a className="mr-5 text-xl hover:text-gray-900">한줄평</a>
                         </Link>
                     </nav>
-                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">그냥 버튼
+                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">사랑합니다
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>
